67325de52bbdcd06813b33d619da117800b5a317
Mahmud <mahmudur69@student.sust.edu>
2022-05-21 00:10:11 +0600

    array number sorting with iife function

--- Practice JavaScript/tutorial26/index.js
+++ Practice JavaScript/tutorial26/index.js
@@ -1,13 +1,22 @@
-var num = new Array;
-var sum = 0;
-for(var i=0; i<5; i++){
+// var num = new Array;
+// var sum = 0;
+// for(var i=0; i<5; i++){
 
-    num[i] = parseInt( prompt("Enter number : ")) ;
+//     num[i] = parseInt( prompt("Enter number : ")) ;
 
-    sum = sum + num[i];
+//     sum = sum + num[i];
 
-    console.log(num[i]);
-}
+//     console.log(num[i]);
+// }
 
-console.log(sum);
+// console.log(sum);
+
+var num = [5, 20, 3, 2, 6, 9, 19];
+
+num.sort(function(a, b){
+    return b-a;
+});
+
+console.log(num);
+
 
