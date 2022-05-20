a2e64528dcb169a05f6d5b354adb7420ae333f6d
Mahmud <mahmudur69@student.sust.edu>
2022-05-20 23:54:11 +0600

    sum of number

---
+++ Practice JavaScript/tutorial26/index.js
@@ -0,0 +1,13 @@
+var num = new Array;
+var sum = 0;
+for(var i=0; i<5; i++){
+
+    num[i] = parseInt( prompt("Enter number : ")) ;
+
+    sum = sum + num[i];
+
+    console.log(num[i]);
+}
+
+console.log(sum);
+
